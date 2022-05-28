10d3cb9b0d981f3a43763f3cc85297d7a64c567a
Mickey <mickeyc11111@gmail.com>
2022-05-28 12:24:45 -0400

    task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector('form')
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      let input = document.querySelector('#new-task-description').value;
+      newTask(input);
+      form.reset();
+    });
 });
+
+function newTask(task) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  p.textContent = `${task} `;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p)
+  ;
+}
+
+function handleDelete (e) {
+  e.target.parentNode.remove();
+}
